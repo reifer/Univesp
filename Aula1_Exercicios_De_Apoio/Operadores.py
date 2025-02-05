8e910148d47e37accf6c2da3f29d91b7e8123e79
reifer <reinaldofsjunior@gmail.com>
2025-02-05 08:16:57 -0300

    Exercicios de Apoio da Semana 2.

--- Aula1_Exercicios_De_Apoio/Operadores.py
+++ Aula1_Exercicios_De_Apoio/Operadores.py
@@ -1,11 +1,11 @@
 # Define as variáveis a, b e c com valores inteiros e de ponto flutuante
-a = 10  # Variável inteira
-b = 3   # Variável inteira
+a = 10    # Variável inteira
+b = 3     # Variável inteira
 c = 10.5  # Variável de ponto flutuante
 
 # Imprime os resultados das operações
-print("a / b = ", a/b)  # Imprime o resultado da divisão de a por b
-print("a // b = ", a//b)  # Imprime o resultado da divisão inteira de a por b
-print("a % b = ", a % b)  # Imprime o resto da divisão de a por b
+print("a / b = ", a/b)     # Imprime o resultado da divisão de a por b
+print("a // b = ", a//b)   # Imprime o resultado da divisão inteira de a por b
+print("a % b = ", a % b)   # Imprime o resto da divisão de a por b
 print("c // b = ", c / b)  # Imprime o resultado da divisão de c por b
-print("c % b = ", c % b)  # Imprime o resultado da divisão inteira de c por b
+print("c % b = ", c % b)   # Imprime o resultado da divisão inteira de c por b
---
+++ Avaliativas/Av2_4.py
@@ -0,0 +1,29 @@
+# #max(nota1, nota2, nota3, nota4, nota5, nota6).
+# m = max(10, 8, 7, 5, 3, 2)
+# print(m)
+#
+# notas = (10, 8, 7, 5, 3, 20)
+# max(notas)
+# print(notas)
+#
+# a = 5
+# b = 4
+# print("Resposta: ", a % b != 1)
+# print("Resp", a % b)
+
+numeroMegaSena = 8
+acertou = False
+print(acertou)
+a = not acertou
+print(a)
+# chute = int(input("Digite um número: "))
+#
+# if chute == numeroMegaSena:
+#     acertou = True
+# else:
+#     if not acertou:
+#         print("Você não adivinhou o número!")
+#     else:
+#         print("Você adivinhou o número!")
+
+
